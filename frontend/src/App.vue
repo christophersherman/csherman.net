6f52cb14388e13cc9f47ca28f0f18fea36192cd0
Christopher Sherman <christopher.sherman@qantasloyalty.com>
2023-11-24 11:09:23 +1100

    First commit - containers

---
+++ frontend/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      message: ''
+    }
+  },
+  created() {
+    axios.get('http://localhost:5001/')
+      .then(response => {
+        this.message = response.data;
+      })
+      .catch(error => {
+        console.error('There was an error!', error);
+      });
+  }
+}
+</script>
