2c05f8657665668046d92ccb8b62efbfb0f97362
Christopher Sherman <csherman@mailbox.org>
2025-05-17 10:19:10 -0700

    host resume instead

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
-import Resume from './Components/Resume';
 import Contact from './Components/Contact';
+import Resume from './Components/Resume';
 import Testimonials from './Components/Testimonials';
-import Portfolio from './Components/Portfolio';
 import { inject } from '@vercel/analytics';
 class App extends Component {
 
@@ -49,7 +48,6 @@ class App extends Component {
       <div className="App">
         <Header data={this.state.resumeData.main}/>
         <About data={this.state.resumeData.main}/>
-        <Portfolio data={this.state.resumeData.portfolio}/>
         <Resume data={this.state.resumeData.resume}/>
         {/* <Portfolio data={this.state.resumeData.portfolio}/>
          <Testimonials data={this.state.resumeData.testimonials}/>
